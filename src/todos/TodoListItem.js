f1a7570e9f365b25cb92410fd56b0d7fbb7864fd
GarrafaTati <tatiigarrafa@gmail.com>
2023-08-08 10:42:14 -0400

    testing styled-components

--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -10,11 +10,13 @@ const TodoItemContainer = styled.div`
   box-shadow: 0 4px 8px grey;
 `;
 
-const TodoItemContainerWithWarning = styled(TodoItemContainer)`
-  border-bottom: ${(props) => (new Date(props.createdAt) > new Date(Date.now() - 8640000 * 5) ? "none" : "2px solid red")};
-`;
-//8640000 = 1 day in milisecs
+export const getBorderStyleForDate = (startingDate, currentDate) => (startingDate > new Date(currentDate - 86400000 * 5) ? "none" : "2px solid red");
+//86400000 = 1 day in milisecs
 //5 days
+
+const TodoItemContainerWithWarning = styled(TodoItemContainer)`
+  border-bottom: ${(props) => getBorderStyleForDate(new Date(props.createdAt), Date.now())};
+`;
 
 const ButtonsContainer = styled.div`
   position: absolute;
